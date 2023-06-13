aca02f7e67ab50dc1d72f7e3f700f442748e2d4b
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-13 14:12:38 +0545

    generating csv files for the autoencoder dataset

--- Datasets/generateDataset.py
+++ Datasets/generateDataset.py
@@ -3,7 +3,7 @@ import random
 import csv
 
 root_directory = "/home/optimus/Downloads/Dataset/ChestXRays/NIH/images"
-output_directory = "/home/optimus/wiseyak/Chest_XRays/autoencoder_pretraining/"
+output_directory = "/home/optimus/wiseyak/Chest_XRay_Model/autoencoder_pretraining/"
 
 # Get the list of image file names
 image_files = os.listdir(root_directory)
