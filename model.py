a858017bb156772bb512b083414062c5adb6ab96
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-14 16:34:50 +0545

    The model should begin training now

--- model.py
+++ model.py
@@ -25,51 +25,39 @@ class Model():
     def __init__(self, trained=False):
         self.model = ResNet50().to(DEVICE)
 
-    def psnr(self, reconstructed, original, max_val=1.0): return 20 * torch.log10(max_val / torch.sqrt(F.mse_loss(reconstructed, original)))        
+    # def psnr(self, reconstructed, original, max_val=1.0): return 20 * torch.log10(max_val / torch.sqrt(F.mse_loss(reconstructed, original)))        
 
 
     def train(self, dataset, loss_func, optimizer):
 
         self.model.train()
         running_loss = 0.0
-        running_psnr = 0.0
+        running_correct = 0.0
         counter = 0
 
-        for i, img in tqdm(enumerate(dataset), total=len(dataset)):
+        for i, (img, label) in tqdm(enumerate(dataset), total=len(dataset)):
+
             counter += 1
-            image = img.to(DEVICE)
-            aug_image = image
-            if random.random() > 0.5:
-
-                # Horizontally flip the image, 30% of the time
-                if random.random() > 0.7:
-                    # print("Before Flipping: ", aug_image.shape)
-                    aug_image = trF.hflip(aug_image)
-                    # print("After Flipping: ", aug_image.shape)
-                    
-                # Create 2-5 16x16 blackout patches in the image, along random locations in the axis of height and width
-                if random.random() > 0.5:
-                    for _ in range(random.randint(0, 3)):
-                        x = random.randint(0, image.size(2) - 16)
-                        y = random.randint(0, image.size(3) - 16)
-                        aug_image[:, :, x:x + 16, y:y + 16] = 0.0
-
             optimizer.zero_grad()
-            output = self.model(aug_image)
-            loss = loss_func(output[1], image)
-            loss.backward()
-            optimizer.step()
+            image, label = img.to(DEVICE), label.to(DEVICE)
+            outputs = self.model(image)
+            loss = loss_func(outputs, label)
             running_loss += loss.item()
 
-            # Calculate the Jaccard score here
-            psnr = self.psnr(output[1], image)
-            running_psnr += psnr.item()
+            loss.backward()
+            optimizer.step()
 
+            # calculate accuracy
+            pred = outputs.argmax(1)
+            # print(label, pred)
+            correct = pred == label
+            running_correct += correct.sum().item()
 
-        epoch_loss = running_loss / (counter * BATCH_SIZE)
-        epoch_psnr = running_psnr / counter
+        # loss and accuracy for a complete epoch
+        epoch_loss = running_loss / (counter*BATCH_SIZE)
+        epoch_acc = 100. * (running_correct / (counter*BATCH_SIZE))
 
-        return epoch_loss, epoch_psnr
+        return epoch_loss, epoch_acc
 
 
 
@@ -78,63 +66,63 @@ class Model():
 
         self.model.eval()
         running_correct = 0.0
-        running_psnr = 0.0
         counter = 0
 
         with torch.no_grad():
-            for i, img in tqdm(enumerate(dataset), total=len(dataset)):
+            for i, (img, label) in tqdm(enumerate(dataset), total=len(dataset)):
                 counter += 1
-                img = img.to(DEVICE)
-                output = self.model(img)
+                img, label = img.to(DEVICE), label.to(DEVICE)
+                outputs = self.model(img)
 
-                psnr = self.psnr(output[1], img)
-                running_psnr += psnr.item()
+                #calculate accuracy
+                pred = outputs.argmax(1)
+                correct = pred == label
+                running_correct += correct.sum().item()
+
+        # loss and accuracy for a complete epoch
+        epoch_acc = 100. * (running_correct / (counter*BATCH_SIZE))
+        return epoch_acc
+
+
+
+    def test(self, dataset):
+
+        # self.model.load_state_dict(torch.load('saved_model/TOMATO_LEAF_PLANTVILLAGE_EFFICIENTNET_10CLASSES_V1_3_200.pth'))
+        running_correct = 0.0
+        counter = 0
+
+        # num = random.randint(0, len(dataset)-1)
+        self.model.eval()
+        with torch.no_grad():
+            for i, (img, label) in tqdm(enumerate(dataset), total=len(dataset)):
+                counter += 1
+                img, label = img.to(DEVICE), label.to(DEVICE)
+                outputs = self.model(img)
+                #calculate accuracy
+                pred = outputs.argmax(1)
+                correct = pred == label
+                running_correct += correct.sum().item()
+                
+                # if i == num:
+                #     try:
+                #         os.makedirs(f"saved_samples/{MODEL_NAME}", exist_ok=True)
+                #     except:
+                #         pass
+                #     sample = random.randint(0, BATCH_SIZE//2)
+                #     image = img[sample, :, :, :].cpu().numpy().transpose((1, 2, 0))
+                #     image = (image * 255).astype('uint8')
+                #     image = Image.fromarray(image)
+                #     draw = ImageDraw.Draw(image)
+                #     real_label = self.classes[label[sample].item()]
+                #     pred_label = self.classes[pred[sample].item()]
+                #     draw.text((image.width - 200, 0), f"Real: {real_label}", fill='red')
+                #     draw.text((image.width - 200, 20), f"Predicted: {pred_label}", fill='blue')
+                #     image.save(f"saved_samples/{MODEL_NAME}/{num}.jpg")
+
+        # loss and accuracy for a complete epoch
+        epoch_acc = 100. * (running_correct / (counter))
     
-        epoch_psnr = running_psnr / counter
-        return epoch_psnr
-
-
-
-    def test(self, dataset, epoch):
-        running_psnr = 0.0  
-        counter = 0
-        num = random.randint(0, len(dataset) // (BATCH_SIZE // 2))
-
-        with torch.no_grad():
-            for i, img in tqdm(enumerate(dataset), total=len(dataset)):
-                counter += 1
-                image = img.to(DEVICE)
-                img = image
-                for _ in range(random.randint(0, 3)):
-                    x = random.randint(0, image.size(2) - 32)
-                    y = random.randint(0, image.size(3) - 32)
-                    img[:, :, x:x + 32, y:y + 32] = 0.0
-                output = self.model(img)
-                pred = output[1]
-                psnr = self.psnr(output[1], image)  
-                running_psnr += psnr.item()
-
-                if i == num:
-                    try:
-                        os.makedirs(f"saved_samples/{MODEL_NAME}", exist_ok=True)
-                    except:
-                        pass
-                    image = img[0].cpu().numpy().transpose((1, 2, 0))
-                    pred = pred[0].cpu().numpy().transpose((1, 2, 0))
-                    image = (image * 255).astype('uint8')
-                    pred = (pred * 255).astype('uint8')
-                    image_pil = Image.fromarray(image)
-                    pred_pil = Image.fromarray(pred)
-                    # image_pil.save(f"saved_samples/{MODEL_NAME}/{epoch}_image.jpg")
-                    # pred_pil.save(f"saved_samples/{MODEL_NAME}/{epoch}_pred.jpg")
-                    stacked_image = Image.new('RGB', (image_pil.width * 2, image_pil.height))
-                    stacked_image.paste(image_pil, (0, 0))
-                    stacked_image.paste(pred_pil, (image_pil.width, 0))
-                    stacked_image.save("stacked_image.jpg")
-                    stacked_image.save(f"saved_samples/{MODEL_NAME}/{epoch}.jpg")
-
-        epoch_psnr = running_psnr / counter 
-        return epoch_psnr
+        return epoch_acc
 
 
 
@@ -155,58 +143,56 @@ class Model():
         optimizer = optim.AdamW(self.model.parameters(), lr)
         print(f"Beginning to train...")
 
-        # mixedloss = MixedLoss(0.5, 0.5)
-        l2loss = torch.nn.MSELoss()
-
-        val_psnr_epochs = []
+        crossEntropyLoss = nn.CrossEntropyLoss()
+        train_loss_epochs, val_acc_epochs, test_acc_epochs = [], [], []
         writer = SummaryWriter(f'runs/{MODEL_NAME}/')
-        os.makedirs(f"{large_file_dir}checkpoints/", exist_ok=True)
-        os.makedirs(f"{large_file_dir}saved_model/", exist_ok=True)
+        os.makedirs("checkpoints/", exist_ok=True)
+        os.makedirs("saved_model/", exist_ok=True)
 
 
         for epoch in range(1, epochs+1):
 
             print(f"Epoch No: {epoch}")
+            train_loss, train_acc = self.train(dataset=train_data, loss_func=crossEntropyLoss, optimizer=optimizer)
+            val_acc = self.validate(dataset=val_data)
+            test_acc = self.test(dataset=test_data)
+            train_loss_epochs.append(train_loss)
+            val_acc_epochs.append(val_acc)
+            test_acc_epochs.append(test_acc)
+            print(f"Train Loss:{train_loss}, Train Accuracy:{train_acc}, Validation Accuracy:{val_acc}, Test Accuracy: {test_acc}")
+            print(f"Test Accuracy: {test_acc}")
 
-            train_loss, train_psnr = self.train(dataset=train_data, loss_func=l2loss, optimizer=optimizer)
+            if max(test_acc_epochs) == test_acc:
+                torch.save({
+                'epoch': epoch,
+                'model_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': train_loss,
+                }, f"checkpoints/{MODEL_NAME}.tar")
 
-            val_psnr = self.validate(dataset=val_data)
-            val_psnr_epochs.append(val_psnr)
-
-            print(f"Train Loss:{train_loss}, Train Jaccard Score:{train_psnr}, Validation Jaccard Score:{val_psnr}")
-
-            writer.add_scalar("Loss/Train", train_loss, epoch)
-            writer.add_scalar("PSNR/Train", train_psnr, epoch)
-            writer.add_scalar("PSNR/Val", val_psnr, epoch)
-
-
-            if max(val_psnr_epochs) == val_psnr:
-                torch.save(self.model.state_dict(), f"{large_file_dir}checkpoints/{MODEL_NAME}.pth")
+            writer.add_scalar("Loss/train", train_loss, epoch)
+            writer.add_scalar("Accuracy/train", train_acc, epoch)
+            writer.add_scalar("Accuracy/val", val_acc, epoch)
+            writer.add_scalar("Accuracy/Test", test_acc, epoch)
             
-            if epoch%5==0:
-                print("Saving model")
-                torch.save(self.model.state_dict(), f"{large_file_dir}saved_model/{MODEL_NAME}_{epoch}.pth")
-                test_psnr = self.test(test_data, epoch)
-                writer.add_scalar("PSNR/Test", test_psnr)
-                print("Model Saved")
-
+            print("Saving model")
+            torch.save(self.model.state_dict(), f"saved_model/{MODEL_NAME}_{epoch}.pth")
+            print("Model Saved")
     
             print("Epoch Completed. Proceeding to next epoch...")
 
         print(f"Training Completed for {epochs} epochs.")
 
 
-    def infer_a_random_sample(self):
-        
-        try:
-            os.makedirs(f"test_samples/{MODEL_NAME}", exist_ok=True)
-        except:
-            pass
-        
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-        ])
+    def infer_a_sample(self, image):
+
+        image = image.to(DEVICE)
+        self.model.eval()
+        # Forward pass the image through the model.
+        prediction = nn.Softmax(dim=1)(self.model(image)).max(1)
+        class_prob, class_index = round(prediction.values.item(), 3), prediction.indices.item()
+        class_name = self.classes[class_index]
+        return f'{class_name}: {class_prob*100}%'
 
 
 
