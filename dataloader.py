50c1ed01bdcf2c30329bb7153e78c0a0b21ba199
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-15 10:26:34 +0545

    Making the model save samples with class named associated and attached

--- dataloader.py
+++ dataloader.py
@@ -18,7 +18,7 @@ class ChestXRayDataset(Dataset):
         ])
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data[:320])
 
     def __getitem__(self, index):
         row = self.data.iloc[index]
