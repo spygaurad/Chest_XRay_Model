5e6ea65f5da684a508e71ff0d752b07f685415a2
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-15 12:50:27 +0545

    Trying out the GradCAM

--- network.py
+++ network.py
@@ -15,7 +15,7 @@ class EfficientNet(nn.Module):
         self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
-        if x[1] == 256:
+        if x.shape[1] == 256:
             x.permute(0, 3, 2, 1)
         x = self.effnet(x)
         x = self.relu(self.fc2(self.relu(self.fc1(x))))
