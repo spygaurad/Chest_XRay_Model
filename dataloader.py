6be41dd518579fca12767894322161c9b9ea289a
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-16 10:51:33 +0545

    added binary cross entropy loss function

--- dataloader.py
+++ dataloader.py
@@ -20,7 +20,7 @@ class ChestXRayDataset(Dataset):
         ])
         
     def __len__(self):
-        return len(self.data)
+        return len(self.data[:0.2])
     
     def __getitem__(self, index):
         row = self.data[index]
