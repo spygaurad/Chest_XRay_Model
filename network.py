ab326d22bc068ff75803184c50b032794df81dc6
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-20 12:08:25 +0545

    removed an extra layer from the model. Effiecnt net as a backbone returns 64 length vector

--- network.py
+++ network.py
@@ -6,19 +6,18 @@ import torch
 class EfficientNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.effnet = timm.create_model('tf_efficientnetv2_b0', num_classes=1024, pretrained=False)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 64)
-        self.classficationLayer = nn.Linear(64, 16)
+        self.effnet = timm.create_model('tf_efficientnetv2_b0', num_classes=64, pretrained=False)
+        self.fc1 = nn.Linear(64, 32)
+        self.classficationLayer = nn.Linear(32, 16)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
         x = self.effnet(x)
-        x = self.relu(self.fc2(self.relu(self.fc1(x))))
+        x = self.relu(self.fc1(x))
         return  self.sigmoid(self.classficationLayer(x))
 
-# inp = torch.rand((1, 3, 256, 256))
-# model = ResNet50()
-# out = model(inp)
-# print(out.shape)
+inp = torch.rand((1, 3, 256, 256))
+model = EfficientNet()
+out = model(inp)
+print(out.shape)
