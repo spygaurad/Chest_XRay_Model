67bfcf29cef700541e4df59530f766b876c22b85
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-21 14:23:16 +0545

    making changes in the model as in with the data augmentations

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ from metrics import DiceLoss, MixedLoss
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # DEVICE = "cpu"
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 MODEL_NAME = "EfficientNet_1"
 large_file_dir = '/mnt/media/wiseyak/Chest_XRays/'
 
