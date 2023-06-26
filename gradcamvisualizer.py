10c29888030a1e6157d05ec0718d2816470fc417
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-26 13:18:46 +0545

    training the model on the side

--- gradcamvisualizer.py
+++ gradcamvisualizer.py
@@ -26,40 +26,38 @@ class GradCAM:
         def forward_hook(module, input, output):
             self.feature_map = output.detach()
 
-        target_layer.register_full_backward_hook(backward_hook)
         target_layer.register_forward_hook(forward_hook)
+        target_layer.register_backward_hook(backward_hook)
 
     def forward(self, input_image):
         return self.model(input_image)
 
     def backward(self, target_class):
+        if self.gradient is None:
+            raise RuntimeError("Gradient is not available. Make sure to call forward() before backward().")
 
-            gradients = torch.mean(self.gradient, dim=(2, 3))
-            target_feature_map = self.feature_map
+        gradients = torch.mean(self.gradient, dim=(2, 3))
+        target_feature_map = self.feature_map
+        weights = torch.sum(gradients * target_feature_map, dim=1)
+        weights = torch.relu(weights)
 
-            # Compute the gradient of the feature maps
-            weights = gradients * target_feature_map * target_class
-            weights = torch.relu(weights)
+        # Perform global average pooling on the weights
+        weights = torch.mean(weights, dim=(1, 2))
 
-            # Perform global average pooling on the weights
-            weights = torch.mean(weights, dim=(1, 2))
+        # Expand dimensions for compatibility with the feature map size
+        weights = weights.unsqueeze(1).unsqueeze(2)
 
-            # Expand dimensions for compatibility with the feature map size
-            weights = weights.unsqueeze(1).unsqueeze(2)
+        # Obtain the weighted combination of the feature maps
+        grad_cam = torch.sum(weights * target_feature_map, dim=0)
 
-            # Obtain the weighted combination of the feature maps
-            grad_cam = torch.sum(weights * target_feature_map, dim=0)
+        # Apply ReLU to eliminate negative values
+        grad_cam = torch.relu(grad_cam)
 
-            # Generate the heatmap
-            heatmap = grad_cam.generate_heatmap(grad_cam)
+        # Normalize the grad_cam values between 0 and 1
+        grad_cam = grad_cam - torch.min(grad_cam)
+        grad_cam = grad_cam / torch.max(grad_cam)
 
-            # Overlay the heatmap on the original image
-            overlay = grad_cam.overlay_heatmap(image, heatmap)
-
-            # Save the visualization images
-            heatmap.save(f'heatmap_class_{class_idx}.jpg')
-            overlay.save(f'overlay_class_{class_idx}.jpg')
-
+        return grad_cam
 
     def generate_heatmap(self, grad_cam):
         heatmap = cv2.applyColorMap(np.uint8(255 * grad_cam.cpu()), cv2.COLORMAP_JET)
