425b9164988a316a57cc734b0082d3827d91d823
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-26 17:09:05 +0545

    training the model on the side

--- model.py
+++ model.py
@@ -169,7 +169,7 @@ class Model():
         weight_tensor = class_weights.to(DEVICE)
 
         print("Dataset Loaded.")
-        binaryCrossEntropyLoss = nn.BCEWithLogitsLoss(weight=weight_tensor)
+        binaryCrossEntropyLoss = nn.BCEWithLogitsLoss()
         # bceLoss = nn.BCELoss()
         # mseloss = nn.MSELoss()
 
