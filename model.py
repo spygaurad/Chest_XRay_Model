1fa5a53da43973c8cfe8c7aaedf82bda140f5c85
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-27 13:02:39 +0545

    just testing the model

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ from metrics import DiceLoss, MixedLoss
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # DEVICE = "cpu"
-BATCH_SIZE = 64
+BATCH_SIZE = 1
 MODEL_NAME = "EfficientNet_1"
 large_file_dir = '/mnt/media/wiseyak/Chest_XRays/'
 
