b2c5904215812afc477a81b45e260aa497860e4b
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-28 22:48:33 +0545

    training with a smaller sample of NIH dataset

--- dataloader.py
+++ dataloader.py
@@ -166,7 +166,7 @@ class ChestXRayDataset(Dataset):
         self.weight_tensor = self._calculate_class_weights()
 
     def __len__(self):
-        return len(self.data[:])
+        return len(self.data[:320])
 
     def __getitem__(self, index):
         row = self.data[index]
