bd110ea6f6c08a6190173685fe2557cc71687909
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-28 16:39:05 +0545

    Training in onelabel setup in smaller chunk of full dataste

--- dataloader.py
+++ dataloader.py
@@ -21,7 +21,7 @@ class ChestXRayDataset(Dataset):
         self.weight_tensor = self._calculate_class_weights()
 
     def __len__(self):
-        return len(self.data[:320])
+        return len(self.data[:])
 
     def __getitem__(self, index):
         row = self.data[index]
