48997dbc68acabeab0993c35d5df45ec267571d8
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-29 11:10:34 +0545

    adding 2 initial layers

--- network.py
+++ network.py
@@ -6,7 +6,8 @@ import torch
 class EfficientNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.preConv = nn.Conv2d(1, 3, 3, 1, padding='same')
+        self.preConv1 = nn.Conv2d(1, 3, 3, 1, padding='same')
+        self.preConv2 = nn.Conv2d(3, 3, 3, 1, padding='same')
         self.effnet = timm.create_model('tf_efficientnetv2_b0', num_classes=64, pretrained=True)
         self.fc1 = nn.Linear(64, 32)
         self.classficationLayer = nn.Linear(32, 16)
@@ -14,7 +15,7 @@ class EfficientNet(nn.Module):
         self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
-        x = self.relu(self.preConv(x))
+        x = self.relu(self.preConv2(self.relu(self.preConv1(x))))
         features = self.effnet(x)
         x = self.fc1(features)
         return  features, self.sigmoid(self.classficationLayer(x))
