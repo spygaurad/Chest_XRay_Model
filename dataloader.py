27f8732656f74cd5fbdeb30136acfe2d3447fcf6
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-29 11:11:57 +0545

    adding 2 initial layers

--- dataloader.py
+++ dataloader.py
@@ -167,7 +167,7 @@ class ChestXRayDataset(Dataset):
         self.weight_tensor = self._calculate_class_weights()
 
     def __len__(self):
-        return len(self.data[:100])
+        return len(self.data[:])
 
     def __getitem__(self, index):
         row = self.data[index]
